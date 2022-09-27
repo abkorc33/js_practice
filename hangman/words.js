f9b0d35dadaf3b58fafb8d8b0261d75383ecccf9
Kimsuhyeon <104416767+abkorc33@users.noreply.github.com>
2022-09-27 18:11:02 +0900

    Add files via upload

---
+++ hangman/words.js
@@ -0,0 +1,5 @@
+let words = [
+    "java", "blight","kerfuffle","development", "script", "interface", "library", "document ",
+    "handling", "event", "animation", "much", "simpler", "across", "multitude", "combination",
+    "application", "manipulation", "extensibility", "element", "with", "click", "local", "foundation"
+];
